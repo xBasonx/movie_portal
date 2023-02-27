3a02d1eeaed482d19a45c75a4fc121f66693dc35
Kacper <trec2902@gmail.com>
2023-02-27 17:03:46 +0100

    update 27.02

--- filmy/urls.py
+++ filmy/urls.py
@@ -10,20 +10,3 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),#połączenie dwóch adresów url , wyświetlanie loginu gdy sie wylogujemy
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-from django.urls import path
-from filmyweb.views import test_response
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('test/', test_response )
-]
-
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('filmy/', include('filmyweb.urls'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- filmyweb/urls.py
+++ filmyweb/urls.py
@@ -7,11 +7,3 @@ urlpatterns = [
     path('edytuj/<int:id>/', edytuj_film ,name="edytuj_film"),
     path('usun/<int:id>/', usun_film, name="usun_film"),
 ]
-
-from filmyweb.views import wszystkie_filmy , nowy_film, edytuj_film
-
-urlpatterns = [
-    path('wszystkie/', wszystkie_filmy),
-    path('nowy/', nowy_film),
-    path('edytuj/<int:id>/', edytuj_film),
-]
