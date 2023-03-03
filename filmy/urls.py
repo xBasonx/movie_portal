c69fb755b1c0127843a465304e5ddd494ccd35a2
Kacper <trec2902@gmail.com>
2023-03-03 17:12:16 +0100

    03,03 fix problem

--- filmy/urls.py
+++ filmy/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('filmy/', include('filmyweb.urls')),
